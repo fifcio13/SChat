d54a34cd47205de739d938130e27cd7317363788
fifcio13 <filipson20@gmail.com>
2020-09-02 19:41:50 +0200

    Spolszczenie

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            anonymo<span>U</span>s <span>Chat</span>. Made by Filip Krawczyk
+            anonymo<span>U</span>s <span>Chat</span>. Stworzone przez - Filip Krawczyk
         </footer>
     )
 }
